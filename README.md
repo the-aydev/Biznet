73b4d7e0d5ae6366cadf4ed14a54cc2a71707ad1
Ayomide <ayomidedans@gmail.com>
2022-02-12 23:37:58 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A taste of **Tailwindcss**
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+  },
+  plugins: [],
+}
